ffa940cf815f23916bedad93aadf315b85a8ee38
Edric Emmanuel Sia <edric.sia@gmail.com>
2021-09-03 19:44:24 +0800

    Fixed table layout

--- my-app/src/components/UserRow.js
+++ my-app/src/components/UserRow.js
@@ -2,13 +2,13 @@ import React from "react";
 
 export default function UserRow({ client }) {
   return (
-    <div>
+    <>
       <tr>
         <td>{client.accountNo}</td>
         <td>{client.firstName}</td>
         <td>{client.lastName}</td>
         <td>{client.balance}</td>
       </tr>
-    </div>
+    </>
   );
 }
