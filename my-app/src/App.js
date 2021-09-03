e669d785bfee07e84037707ec15321978090b944
jason-nn <jason.ho@obf.ateneo.edu>
2021-09-03 19:19:57 +0800

    added userrow

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,96 +1,117 @@
-import React, { useState } from 'react';
-import './App.css';
-import Login from './components/Login.js';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import AdminView from './components/AdminView';
-import Deposit from './components/Deposit';
-import Withdraw from './components/Withdraw';
-import Transfer from './components/Transfer';
-
+import React, { useState } from "react";
+import "./App.css";
+import Login from "./components/Login.js";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import AdminView from "./components/AdminView";
+import Deposit from "./components/Deposit";
+import Withdraw from "./components/Withdraw";
+import Transfer from "./components/Transfer";
 
 function App() {
-
   //for admin user details, planning to merge this with users
   const adminUser = {
     username: "admin",
     password: "pass123",
     name: "Administrator",
-    isAdmin: true
+    isAdmin: true,
   };
 
   // list of users (work in progress)
   const userList = [
     {
       accountNo: null,
-      name: "Administrator",
+      firstName: "ADMINISTRATOR",
+      lastName: "",
       balance: null,
       username: "admin",
       password: "pass123",
-      isAdmin: true
+      isAdmin: true,
     },
     {
-      accountNo: 9000000,
-      name: "Sample",
-      balance: 123,
+      accountNo: 12345678,
+      firstName: "JUAN",
+      lastName: "DE LA CRUZ",
+      balance: 2000,
       username: "sample",
       password: "pass123",
-      isAdmin: false
+      isAdmin: false,
     },
-  ]
+    {
+      accountNo: 12345679,
+      firstName: "JASON",
+      lastName: "HO",
+      balance: 6900,
+      username: "jason",
+      password: "jason123",
+      isAdmin: false,
+    },
+    {
+      accountNo: 12345689,
+      firstName: "EMAN",
+      lastName: "SIA",
+      balance: 4200,
+      username: "eman",
+      password: "eman123",
+      isAdmin: false,
+    },
+  ];
 
   const [users, setUserList] = useState(userList);
 
-
-
-  //state for user details 
+  //state for user details
   const [currentUser, setUser] = useState({ name: "", username: "" });
 
   //state for error message if login failed
   const [error, setError] = useState("");
 
   //function for logging in
-  const LoginFunction = details => {
+  const LoginFunction = (details) => {
     console.log(details);
-    if (details.username === adminUser.username && details.password === adminUser.password) {
+    if (
+      details.username === adminUser.username &&
+      details.password === adminUser.password
+    ) {
       setUser({
         name: adminUser.name,
-        username: adminUser.username
+        username: adminUser.username,
       });
       setError("");
     } else {
-      setError('Login failed. Please try again.');
+      setError("Login failed. Please try again.");
     }
-  }
+  };
 
   //function for logging out
   const Logout = () => {
-    console.log('Logout');
+    console.log("Logout");
     setUser({ name: "", username: "" });
-  }
+  };
 
   return (
     <div>
       {/* If the user info is not blank, show dashboard */}
-      {(currentUser.username !== "") ? (
-
+      {currentUser.username !== "" ? (
         <Router>
           <Navbar LogoutFunction={Logout} />
           <Switch>
-            <Route path="/" exact component={() => (<AdminView name={currentUser.name} users={users} />)} />
+            <Route
+              path="/"
+              exact
+              component={() => (
+                <AdminView name={currentUser.name} users={users} />
+              )}
+            />
             <Route path="/deposit" component={Deposit} />
             <Route path="/withdraw" component={Withdraw} />
             <Route path="/transfer" component={Transfer} />
           </Switch>
         </Router>
-
-      ) :
+      ) : (
         /* If there is no current user, show login page */
-        (
-          <Login LoginFunction={LoginFunction} error={error} />
-        )
-      }
-    </div >
+        <Login LoginFunction={LoginFunction} error={error} />
+      )}
+    </div>
   );
 }
 
--- my-app/src/components/AdminView.js
+++ my-app/src/components/AdminView.js
@@ -1,29 +1,35 @@
 import "./AdminView.css";
 import Button from "./Button";
+import UserRow from "./UserRow";
 
 const AdminView = ({ name, users }) => {
   const nonAdminUsers = users.filter((user) => !user.isAdmin);
+
+  function renderRows() {
+    const rows = [];
+    for (let i = 0; i < nonAdminUsers.length; i++) {
+      rows.push(
+        <UserRow key={nonAdminUsers[i].firstName} client={nonAdminUsers[i]} />
+      );
+    }
+    return rows;
+  }
 
   return (
     <div>
       <h1>Welcome, {name}</h1>
       <h2>Accounts</h2>
-      <Button text="+" />
+      <Button text="Add User" />
       <table>
         <thead>
           <tr>
             <th>Account Number</th>
-            <th>Name</th>
+            <th>First Name</th>
+            <th>Last Name</th>
             <th>Current Balance</th>
           </tr>
         </thead>
-        <tbody>
-          <tr>
-            <td>234567</td>
-            <td>Juan de la Cruz</td>
-            <td>$999</td>
-          </tr>
-        </tbody>
+        <tbody>{renderRows()}</tbody>
       </table>
     </div>
   );
