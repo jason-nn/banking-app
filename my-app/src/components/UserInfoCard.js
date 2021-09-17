0f6b98699089beaebbc818644f8dbfa06fb85702
Edric Emmanuel Sia <edric.sia@gmail.com>
2021-09-17 20:33:50 +0800

    Fixed margins

--- my-app/src/components/UserInfoCard.js
+++ my-app/src/components/UserInfoCard.js
@@ -7,11 +7,14 @@ export default function UserInfoCard({ currentUser }) {
                 <div className='main-header'>
                     <h3 className='main-title'>Your Account</h3>
                 </div>
-                <h3>
-                    {currentUser.firstName} {currentUser.lastName}
-                </h3>
-                <h4>{currentUser.accountNo} </h4>
+                <div className='user-account-name'>
+                    <h3>
+                        {currentUser.firstName} {currentUser.lastName}
+                    </h3>
+                    <h4>{currentUser.accountNo} </h4>
+                </div>
             </div>
+
             <div className='user-account-balance'>
                 <div className='main-header'>
                     <h3 className='main-title'>Current Balance</h3>
